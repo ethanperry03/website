64ec9bb896c7efa31ddca366b24750435b5ec4c6
Ethan Perry <ethan.perry03@gmail.com>
2025-02-06 14:12:16 -0500

    Cleaned up working model of elevation gain calculator for responsive design

--- gain/calculate.js
+++ gain/calculate.js
@@ -1,3 +1,4 @@
+var isDistance = true;
 
 // incline feet, distance feet
 function calculate(incline, dist) {
@@ -10,39 +11,62 @@ $(document).ready(function() {
 
     // event handling clicking which form to show
     $(".distFormButton").click(() => {
-        $(".speedtimeForm").hide();
-        $(".distForm").show();
+        if(isDistance == false) {
+            $(".distForm").show();
+            $(".speedtimeForm").hide();
+            isDistance = true;
+        }
     })
 
     // event handling clicking which form to show
     $(".speedtimeFormButton").click(() => {
-        $(".distForm").hide();
-        $(".speedtimeForm").show();
+        if(isDistance == true) {
+            $(".distForm").hide();
+            $(".speedtimeForm").show();
+            isDistance = false;
+        }
     })
 
 
     // if submit is clicked, call calculate function
-    $("#submit1").click(function(event) {
+    $("#submit").click(function(event){
         event.preventDefault();
-
-        const incline = $("#incline").val();
-        var dist = $("#distance").val() * 5280;
-
+        if(isDistance) {
+            var dist = $("#distance").val() * 5280;
+        }
+        else {
+            // convert from mph to feet / min
+            var speed = $("#speed").val() * 5280 / 60;
+            var time = $("#time").val();
+            var dist = speed * time;
+        }
+        var incline = $("#incline").val();
         var gain = calculate(incline, dist);
         $(".retval").text("Gain: " + gain.toFixed(0) + "ft");
     });
 
-    $("#submit2").click(function(event) {
-        event.preventDefault();
-        // convert from mph to feet / min
-        var speed = $("#speed").val() * 5280 / 60;
-        var time = $("#time").val();
-        const dist = speed * time;
-        const incline = $("#incline").val();
 
-        var gain = calculate(incline, dist);
-        $(".retval").text("Gain: " + gain.toFixed(0) + "ft");
-    });
+    // $("#submit1").click(function(event) {
+    //     event.preventDefault();
+
+    //     const incline = $("#incline").val();
+    //     var dist = $("#distance").val() * 5280;
+
+    //     var gain = calculate(incline, dist);
+    //     $(".retval").text("Gain: " + gain.toFixed(0) + "ft");
+    // });
+
+    // $("#submit2").click(function(event) {
+    //     event.preventDefault();
+    //     // convert from mph to feet / min
+    //     var speed = $("#speed").val() * 5280 / 60;
+    //     var time = $("#time").val();
+    //     const dist = speed * time;
+    //     const incline = $("#incline").val();
+
+    //     var gain = calculate(incline, dist);
+    //     $(".retval").text("Gain: " + gain.toFixed(0) + "ft");
+    // });
 
 
 });
